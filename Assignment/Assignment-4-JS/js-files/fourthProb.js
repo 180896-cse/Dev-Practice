01084b246a43fde53dcaabb3dd7a136316b6a5b1
shantanu <shantanu.pratap@nagarro.com>
2023-04-17 13:08:08 +0530

    initial prototype ready

--- Assignment/Assignment-4-JS/js-files/fourthProb.js
+++ Assignment/Assignment-4-JS/js-files/fourthProb.js
@@ -19,18 +19,29 @@ for(let i=0;i<localStorage.length;i++){
 }
 
 
+
+//Login part code
+
 lgnBtn.addEventListener("click",()=>{
     console.log("LogIn Button Clicked!");
+    if(employePass.value == "" || employeUname.value == ""){
+        alert("Username/Password field cannot be blank!");
+    }
+    else{
     if(localStorage.getItem(employeUname.value) != null){
-        console.log("Username present in LoaclStorage!");
+        console.log("Username present in LocalStorage!");
+        window.location.replace("http://127.0.0.1:5500/Assignment/Assignment-3-JS/");
     }
     else{
         alert("Please Register Username First!");
     }
-    
+}
     
 })
 
+
+
+//Registration Part Code
 
 rgstrBtn.addEventListener("click",()=>{
     console.log("Register Button Clicked!");

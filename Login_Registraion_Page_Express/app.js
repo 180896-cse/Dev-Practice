f53b34bb1d2108639916b0491dca73e18979055e
shantanu <shantanu.pratap@nagarro.com>
2023-04-23 22:04:22 +0530

    connection with atlas mongo db done!

--- Login_Registraion_Page_Express/app.js
+++ Login_Registraion_Page_Express/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const path  = require("path");
+const mongoose = require("mongoose");
+
+//contecting MongoDB ATlas
+const DB = "mongodb+srv://shantanu19nagarro:Shan1234@logindb.lm4a4fh.mongodb.net/samplemongoDb?retryWrites=true&w=majority"
+mongoose.connect(DB).then(()=>{
+  console.log(`connection with database is sucess!`);
+}).catch((err)=>{
+  console.log(`error occured in connection with DB! ${err}`);
+})
+
+
 const app = express();
 
 const port = 3000;
@@ -26,4 +37,3 @@ try {
   console.log(error);
 }
 
-module.exports = app;
--- Login_Registraion_Page_Express/src/Controller/route.js
+++ Login_Registraion_Page_Express/src/Controller/route.js
@@ -2,13 +2,9 @@ const express = require("express");
 const path = require("path");
 const route = express.Router();
 
-
-//from controller itself accessing the static file that have to serve.
-route.get("/",(request,response)=>{
-    response.sendFile(path.join(__dirname, "../View/Htmlfiles/home.html"));
-})
-
-
-
+//From controller itself accessing the static file that have to serve.
+route.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "../View/Htmlfiles/home.html"));
+});
 
 module.exports = route;
